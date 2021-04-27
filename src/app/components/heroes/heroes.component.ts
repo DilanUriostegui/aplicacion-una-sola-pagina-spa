af15606854be837e7afc30a5e43326b89dd93f8c
Dilan <duriostegui@asgbpm.com>
2021-04-27 18:31:38 -0500

    Implementación de la ruta para cada heroe, tarea #1

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
+
+  constructor( private _heroesService: HeroesService,
+              private router: Router 
+              ) { 
+
+   }
 
   ngOnInit(): void {
+
+    this.heroes = this._heroesService.getHeroes();
+
+    // console.log(this.heroes);
+  }
+  verHeroe( idx: number ){
+    this.router.navigate( ['/heroe',idx] );
   }
 
 }
